c82599bc79296b2667bc3f036161bf58593a664c
Brandon Manke <me@brandonmanke.com>
2017-04-06 19:04:05 -0500

    Initial Commit

---
+++ src/it226/group/project/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Main method of our Group Project 2
+ * @author Brandon Manke
+ * @author Colin Koepke
+ * @author Ben Dworkin
+ */
+package it226.group.project;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
